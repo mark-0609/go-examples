1dde13a8cab6b2abb0c956417af23de8fd470850
mzt <823682492@qq.com>
2025-12-30 11:46:51 +0800

    update

--- threading_example/safe_run.go
+++ threading_example/safe_run.go
@@ -2,6 +2,7 @@ package threading_example
 
 import (
 	"fmt"
+	"sync"
 	"time"
 
 	"github.com/zeromicro/go-zero/core/threading"
@@ -29,4 +30,52 @@ func SafeRunDemo() {
 	time.Sleep(time.Second)
 	fmt.Println("主进程依然存活，未受 Panic 影响")
 	fmt.Println("=== 结束 SafeRun 示例 ===")
+}
+
+// downloadFiles 并发下载文件
+func downloadFiles(urls []string) {
+	group := threading.NewRoutineGroup()
+
+	for _, url := range urls {
+		url := url // 创建局部副本
+		group.RunSafe(func() {
+			fmt.Printf("开始下载: %s\n", url)
+			time.Sleep(time.Second) // 模拟下载
+			fmt.Printf("完成下载: %s\n", url)
+		})
+	}
+
+	fmt.Println("等待所有下载完成...")
+	group.Wait()
+	fmt.Println("所有文件下载完成！")
+}
+
+func SafeRunDemo2() {
+	urls := []string{
+		"http://example.com/file1.zip",
+		"http://example.com/file2.zip",
+		"http://example.com/file3.zip",
+	}
+	downloadFiles(urls)
+}
+
+// queryMultipleTables 并发数据库查询
+func queryMultipleTables(tableNames []string) map[string]struct{} {
+	group := threading.NewRoutineGroup()
+	results := make(map[string]struct{})
+	var mu sync.Mutex
+
+	for _, table := range tableNames {
+		table := table
+		group.RunSafe(func() {
+			records := struct{}{}
+
+			mu.Lock()
+			results[table] = records
+			mu.Unlock()
+		})
+	}
+
+	group.Wait()
+	return results
 }
